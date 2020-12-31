ff185936bdcdda74e884fe7f96835d6473073cdc
khang-nd <khang.nguyenduyvn@gmail.com>
2020-12-31 21:37:30 +0700

    Updates

---
+++ script.js
@@ -0,0 +1,36 @@
+const tabs = document.querySelectorAll("menu[role=tablist]");
+
+for (let i = 0; i < tabs.length; i++) {
+  const tab = tabs[i];
+
+  const tabButtons = tab.querySelectorAll("menu[role=tablist] > button");
+
+  tabButtons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      tabButtons.forEach((button) => {
+        if (
+          button.getAttribute("aria-controls") ===
+          e.target.getAttribute("aria-controls")
+        ) {
+          button.setAttribute("aria-selected", true);
+          openTab(e, tab);
+        } else {
+          button.setAttribute("aria-selected", false);
+        }
+      });
+    })
+  );
+}
+
+function openTab(event, tab) {
+  const articles = tab.parentNode.querySelectorAll('[role="tabpanel"]');
+  articles.forEach((p) => {
+    p.setAttribute("hidden", true);
+  });
+  const article = tab.parentNode.querySelector(
+    `[role="tabpanel"]#${event.target.getAttribute("aria-controls")}`
+  );
+  article.removeAttribute("hidden");
+}
