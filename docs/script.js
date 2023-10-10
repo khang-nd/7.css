ce853e2eb52dcb994f7e550584890537aaf0e151
Thuan Hai Cong Ho <hohaicongthuan@gmail.com>
2023-10-10 14:35:27 +0700

    Fix a tab bug

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,7 @@
 // Tabs
-const tabButtons = document.querySelectorAll("[role=tab]");
+// Tabs > Sample Tabs
+const tabList = document.querySelector("[aria-label='Sample Tabs'");
+const tabButtons = tabList.querySelectorAll("[role=tab]");
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener("mousedown", (e) => {
     e.preventDefault();
@@ -18,7 +20,6 @@ tabButtons.forEach((tabButton) => {
       .removeAttribute("hidden");
   });
 });
-
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener("focus", (e) => {
     e.preventDefault();
@@ -37,6 +38,85 @@ tabButtons.forEach((tabButton) => {
   });
 });
 
+// Tabs > Tabs Template
+const tabList_TabsTemp = document.querySelector("[aria-label='Tabs Template'");
+const tabButtons_TabsTemp = tabList_TabsTemp.querySelectorAll("[role=tab]");
+tabButtons_TabsTemp.forEach((tabButton) => {
+  tabButton.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    const tabContainer = e.target.parentElement.parentElement;
+    const targetId = e.target.getAttribute("aria-controls");
+    tabButtons_TabsTemp.forEach((_tabButton) =>
+      _tabButton.setAttribute("aria-selected", false)
+    );
+    tabButton.setAttribute("aria-selected", true);
+    tabButton.focus();
+    tabContainer
+      .querySelectorAll("[role=tabpanel]")
+      .forEach((tabPanel) => tabPanel.setAttribute("hidden", true));
+    tabContainer
+      .querySelector(`[role=tabpanel]#${targetId}`)
+      .removeAttribute("hidden");
+  });
+});
+tabButtons_TabsTemp.forEach((tabButton) => {
+  tabButton.addEventListener("focus", (e) => {
+    e.preventDefault();
+    const tabContainer = e.target.parentElement.parentElement;
+    const targetId = e.target.getAttribute("aria-controls");
+    tabButtons_TabsTemp.forEach((_tabButton) =>
+      _tabButton.setAttribute("aria-selected", false)
+    );
+    tabButton.setAttribute("aria-selected", true);
+    tabContainer
+      .querySelectorAll("[role=tabpanel]")
+      .forEach((tabPanel) => tabPanel.setAttribute("hidden", true));
+    tabContainer
+      .querySelector(`[role=tabpanel]#${targetId}`)
+      .removeAttribute("hidden");
+  });
+});
+
+// Window Body > Window with Tabs
+const tabList_WinWTabs = document.querySelector("[aria-label='Window with Tabs'");
+const tabButtons_WinWTabs = tabList_WinWTabs.querySelectorAll("[role=tab]");
+tabButtons_WinWTabs.forEach((tabButton) => {
+  tabButton.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    const tabContainer = e.target.parentElement.parentElement;
+    const targetId = e.target.getAttribute("aria-controls");
+    tabButtons_WinWTabs.forEach((_tabButton) =>
+      _tabButton.setAttribute("aria-selected", false)
+    );
+    tabButton.setAttribute("aria-selected", true);
+    tabButton.focus();
+    tabContainer
+      .querySelectorAll("[role=tabpanel]")
+      .forEach((tabPanel) => tabPanel.setAttribute("hidden", true));
+    tabContainer
+      .querySelector(`[role=tabpanel]#${targetId}`)
+      .removeAttribute("hidden");
+  });
+});
+tabButtons_WinWTabs.forEach((tabButton) => {
+  tabButton.addEventListener("focus", (e) => {
+    e.preventDefault();
+    const tabContainer = e.target.parentElement.parentElement;
+    const targetId = e.target.getAttribute("aria-controls");
+    tabButtons_WinWTabs.forEach((_tabButton) =>
+      _tabButton.setAttribute("aria-selected", false)
+    );
+    tabButton.setAttribute("aria-selected", true);
+    tabContainer
+      .querySelectorAll("[role=tabpanel]")
+      .forEach((tabPanel) => tabPanel.setAttribute("hidden", true));
+    tabContainer
+      .querySelector(`[role=tabpanel]#${targetId}`)
+      .removeAttribute("hidden");
+  });
+});
+
+
 // Copy code
 document.querySelectorAll(".copy").forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -48,6 +128,7 @@ document.querySelectorAll(".copy").forEach((button) => {
     });
   });
 });
+
 
 // Balloon
 document
