ecdfe5b55ae8867d8c499f3f9b19773a04c23610
Khang <khang.nguyenduyvn@gmail.com>
2025-07-27 13:50:21 +0700

    release: 0.21.1

--- build.js
+++ build.js
@@ -21,28 +21,38 @@ const plugins = [
 
 const { homepage, version } = require("./package.json");
 
-const parser = postcss(plugins);
+let parser = postcss(plugins);
 
-const parserWithPrefix = postcss([
-  ...plugins,
-  require("postcss-prefix-selector")({
-    prefix: ".win7",
-    transform: (prefix, selector, prefixed) => {
-      if ("body" === selector) return selector + prefix;
-      if (":root" === selector) return prefix;
-      return prefixed;
-    },
-  }),
-]);
+/**
+ *
+ * @param {Object} options
+ * @param {boolean} [options.usePrefix] - Whether to apply a prefix for the CSS selectors.
+ * @param {boolean} [options.useInlineVars] - Whether to inline CSS variables.
+ */
+async function buildCSS({ usePrefix, useInlineVars } = {}) {
+  const input = `/*! 7.css v${version} - ${homepage} */\n` + fs.readFileSync("gui/index.scss");
 
-async function buildCSS(usePrefix) {
-  const input =
-    `/*! 7.css v${version} - ${homepage} */\n` +
-    fs.readFileSync("gui/index.scss");
+  let targetFile = "dist/7.css";
+  if (usePrefix) {
+    targetFile = "dist/7.scoped.css";
+    parser = postcss([
+      ...plugins,
+      require("postcss-prefix-selector")({
+        prefix: ".win7",
+        transform: (prefix, selector, prefixed) => {
+          if ("body" === selector) return selector + prefix;
+          if (":root" === selector) return prefix;
+          return prefixed;
+        },
+      }),
+    ]);
+  }
+  if (useInlineVars) {
+    targetFile = "dist/7.inline.css";
+    parser = postcss([...plugins, require("postcss-css-variables")]);
+  }
 
-  const targetFile = usePrefix ? "dist/7.scoped.css" : "dist/7.css";
-
-  const result = await (usePrefix ? parserWithPrefix : parser).process(input, {
+  const result = await parser.process(input, {
     from: "gui/index.scss",
     to: targetFile,
     map: { inline: false },
@@ -62,9 +72,7 @@ function buildComponents() {
       .filter((file) => file.startsWith("_") && file !== variablesFile)
       .map((file) => ({
         name: file.slice(1),
-        content:
-          fs.readFileSync("gui/" + variablesFile) +
-          fs.readFileSync("gui/" + file),
+        content: fs.readFileSync("gui/" + variablesFile) + fs.readFileSync("gui/" + file),
       }));
 
     const parsedResults = fileResults.map(async (file) => {
@@ -81,9 +89,7 @@ function buildComponents() {
 
     mkdirp.sync(targetFolder);
     const results = await Promise.all(parsedResults);
-    results.forEach((result) =>
-      fs.writeFile(result.target, result.parsed.css, () => {})
-    );
+    results.forEach((result) => fs.writeFile(result.target, result.parsed.css, () => {}));
   });
 }
 
@@ -99,10 +105,8 @@ function buildDocs() {
   const template = fs.readFileSync("docs/index.html.ejs", "utf-8");
   const meta = {
     title: "7.css",
-    description:
-      "A design system for building faithful recreations of the Windows 7 UI.",
-    image:
-      "https://raw.githubusercontent.com/khang-nd/7.css/main/docs/window.png",
+    description: "A design system for building faithful recreations of the Windows 7 UI.",
+    image: "https://raw.githubusercontent.com/khang-nd/7.css/main/docs/window.png",
   };
 
   function example(code) {
@@ -121,22 +125,23 @@ function buildDocs() {
     </div>`;
   }
 
-  glob(
-    "docs/*",
-    { ignore: ["docs/components", "docs/sections", "docs/*.ejs"] },
-    (err, files) => {
-      if (!err) {
-        files.forEach((srcFile) =>
-          fs.copyFileSync(srcFile, path.join("dist", path.basename(srcFile)))
-        );
-      } else throw "error globbing dist directory.";
+  glob("docs/*", { ignore: ["docs/components", "docs/sections", "docs/*.ejs"] }, (err, files) => {
+    if (!err) {
+      files.forEach((srcFile) => fs.copyFileSync(srcFile, path.join("dist", path.basename(srcFile))));
+    } else {
+      throw "error globbing dist directory.";
     }
-  );
+  });
   fs.writeFileSync(
     path.join(__dirname, "/dist/index.html"),
     ejs.render(
       template,
-      { getNewId, getCurrentId, meta, example },
+      {
+        getNewId,
+        getCurrentId,
+        meta,
+        example,
+      },
       { views: [path.resolve(__dirname, "./docs")] }
     )
   );
@@ -148,7 +153,8 @@ async function build(mode) {
     buildDocs();
 
     if (mode === "production") {
-      buildCSS(true);
+      buildCSS({ usePrefix: true });
+      buildCSS({ useInlineVars: true });
       buildComponents();
     }
   } catch (err) {
